806667e9d1800e20be18a505f9c5c8b9eb357577
eric-jm-lang <eric.lang@bristol.ac.uk>
2020-04-07 13:57:54 +0100

    finished closed form

---
+++ 2-Closed/2-MD1/prepare_systems_4.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -f
+
+  
+for i in 1 2 3 4 5; do
+
+    cd ./${i}
+
+echo "#!/bin/bash -login
+#
+#SBATCH -p gpu
+#SBATCH -J closed${i}md21-25
+#SBATCH --time=5-00:00:00     # Walltime
+#SBATCH --mail-user=eric.lang@bristol.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1          # number of tasks
+#SBATCH --ntasks-per-node=1 # number of tasks per node
+#SBATCH --gres=gpu:1
+#
+module add OpenMPI/2.0.1-gcccuda-2016.10
+export AMBERHOME=/mnt/storage/scratch/el14718/SOFTWARE/amber16-mofified/amber16
+source /mnt/storage/scratch/el14718/SOFTWARE/amber16-mofified/amber16/amber.sh
+cd \$SLURM_SUBMIT_DIR
+#
+
+old=md20
+for name in md21 md22 md23 md24 md25; do
+  \$AMBERHOME/bin/pmemd.cuda -O -i \$name.i -o closed_${i}_\$name.mdout -p closed10.parm7 -c closed_${i}_\$old.rst7 \
+  -ref closed10.rst7 -x closed_${i}_\$name.nc -r closed_${i}_\$name.rst7 -inf closed_${i}_\$name.mdinfo
+  
+  cp closed_${i}_\$name.rst7 closed_${i}_\$name.rst7.ORG
+  mv closed_${i}_\$name.rst7 TEMP_closed_${i}_\$name.rst7
+  
+  cat > image.in <<EOF
+parm closed10.parm7
+trajin TEMP_closed_${i}_\$name.rst7
+center :1-192 mass origin
+image familiar com :1-192
+trajout closed_${i}_\$name.rst7 restart
+run
+EOF
+  cpptraj < image.in
+  rm image.in
+
+  old=\$name
+done" >  md21-25.slurm 
+    cd ..
+
+done
+
+
+
+
+
+
+
