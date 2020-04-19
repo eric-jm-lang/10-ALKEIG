8cab25d54ac7cdb78e2ba1fccd74b182e2ae426f
eric-jm-lang <eric.lang@bristol.ac.uk>
2020-04-19 12:12:20 +0100

    reimaged traj

---
+++ 7-Closed_IPA/2-MD1/analysis_traj_closed_dry_noeq.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -f
+
+for i in 1 2 3 4 5 6 7 8 9 10 ; do
+  cd $i
+  if [ -f analysis_traj_${i}.in ];
+    then
+    rm analysis_traj_${i}.in
+  fi
+
+  echo "parm closed_ipa_HMR.parm7" >> analysis_traj_${i}.in
+
+  for j in  md1 md2 md3 md4 md5 md6 md7 md8 md9 md10; do
+    echo "trajin ./closed_ipa_${i}_${j}.nc" >> analysis_traj_${i}.in
+  done
+
+echo "center :1-192 mass origin
+image familiar com :1-192
+strip :WAT
+strip :Na+
+strip :Cl-
+strip :IPA
+trajout ../traj_dry_noeq/closed_ipa_dry_${i}.nc
+run
+clear all
+quit" >> analysis_traj_${i}.in
+
+cd ..
+
+done
+
+
+#cpptraj < analysis_traj.in
+
+
