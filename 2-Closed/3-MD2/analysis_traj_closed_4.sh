c397a018e7012c9c95ec07edf888dce7a656d094
eric-jm-lang <eric.lang@bristol.ac.uk>
2020-04-19 12:09:53 +0100

    reimaged traj

---
+++ 2-Closed/3-MD2/analysis_traj_closed_4.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -f
+
+#module add OpenMPI/2.0.1-gcccuda-2016.10
+#export AMBERHOME=/mnt/storage/scratch/el14718/SOFTWARE/amber16-mofified/amber16
+#source /mnt/storage/scratch/el14718/SOFTWARE/amber16-mofified/amber16/amber.sh
+
+for i in 1 2 3 4 5; do
+  cd $i
+  if [ -f analysis_traj_${i}.in ];
+    then
+    rm analysis_traj_${i}.in
+  fi
+
+  echo "parm closed10.parm7" >> analysis_traj_${i}.in
+
+  for j in md1 md2 md3 md4 md5 md6 md7 md8 md9 md10 md11 md12 md13 md14 md15 md16 md17 md18; do
+    echo "trajin ./closed_${i}_${j}.nc" >> analysis_traj_${i}.in
+  done
+
+echo "center :1-192 mass origin
+image familiar com :1-192
+strip :WAT
+strip :Na+
+strip :Cl-
+trajout ../traj_dry_noeq/closed_dry_${i}.nc
+run
+clear all
+quit" >> analysis_traj_${i}.in
+
+cd ..
+
+done
+
+
+#cpptraj < analysis_traj.in
+
+
